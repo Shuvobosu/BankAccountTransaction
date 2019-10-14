b01b618e00da6023ae1d641905cb074cf05823c9
unknown <bosu15-5645@diu.edu.bd>
2019-10-15 00:42:15 +0600

    only create account, deposit and withdraw money

---
+++ AccountOperationAppPractice1/BankAccount.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccountOperationAppPractice1
+{
+    class BankAccount
+    {
+        public double accountNumber;
+        public string customerName;
+        public double amount;
+        public double balance;
+
+        public bool Diposit()
+        {
+            balance = balance + amount;
+            return true;
+        }
+
+        public bool Withdraw()
+        {
+            balance = balance - amount;
+            return true;
+        }
+
+        public string GetReport()
+        {
+            string message = customerName+", " + "your account number: " + accountNumber +" "+ "and its balance: " + balance;
+            return (message);
+        }
+
+
+    }
+}
